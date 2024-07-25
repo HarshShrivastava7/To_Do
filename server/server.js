d15b46ec89d31b317bdc2d3c195075bbaa53e5b1
Harsh Shrivastava <harsh.shrivastava.eee21@itbhu.ac.in>
2024-07-25 07:37:17 +0530

    added vercel.json2

--- server/server.js
+++ server/server.js
@@ -10,5 +10,10 @@ const port = process.env.PORT || 5000;
 const serverRoutes = require("./routes/serverRoutes");
 app.use("/", serverRoutes);
 
+app.use(express.static('client/build'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
 
 app.listen(port, () => console.log(`Node server started at ${port}`));
